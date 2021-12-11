8954818c94ac9fc7f89af808065f38577ed57d37
Quang Tung <bqtung123@gmail.com>
2021-12-11 15:22:42 +0700

    commit dau tien

---
+++ test/controller/TestSuite.java
@@ -0,0 +1,12 @@
+package controller;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+
+{ValidateAddressTest.class}
+)
+public class TestSuite{ // nothing
+}
+
